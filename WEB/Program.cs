c4b4264f6e3e6d56ddd1dc50ab5ef4ffbfa7f1cb
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-09-01 17:37:52 -0300

    feat: sidebar

--- WEB/Program.cs
+++ WEB/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
