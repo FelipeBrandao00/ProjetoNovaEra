f81025e04ea8d9ec5cad5436aa72a60e97dfab2c
FelipeBrandao00 <felipesbrandao13@gmail.com>
2024-10-09 22:28:48 -0300

    interface turma

---
+++ Domain/Interfaces/ITurmaRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces {
+    public interface ITurmaRepository {
+        Task<Turma> AddTurma(Turma turma);
+        Task<Turma> DeleteTurma(Turma turma);
+        Task<Turma> UpdateTurma(Turma turma);
+        Task<Turma> GetTurmas(); //falta os filtros
+        Task<Turma> GetTurmaById(int CdTurma);
+        Task<Turma> FinalizarTurma(int CdTurma);
+        Task<Turma> ReativarTurma(int CdTurma);
+    }
+}
