298ab0fc17cd0a46fe353a77fc2bd87df351c37a
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-10-10 03:09:54 -0300

    feat: tela de inscrição

---
+++ WEB/Controllers/InscricaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB.Models.Inscricao;
+
+namespace WEB.Controllers {
+    public class InscricaoController(IConfiguration configuration) : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmarInscricao(InscricaoViewModel model) {
+            //await model.GerarLista(configuration);
+            //faz a logica de confirmar a insc aqui
+            return View();
+        }
+    }
+}
---
+++ WEB/Models/Inscricao/InscricaoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Models.Inscricao {
+    public class InscricaoViewModel {
+        public string DsNome { get; set; }
+        public string DsEmail { get; set; }
+        public string DsCPF { get; set; }
+        public int CdGenero { get; set; }
+        public DateTime? DtNascimento { get; set; }
+        public string DsTelefone { get; set; }
+        public int CdCurso { get; set; }
+    }
+}
