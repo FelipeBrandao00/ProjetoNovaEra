ca54382f02eaa7a21b5537310e876aa3b7b8622d
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-10-20 18:34:54 -0300

    feat: tela de chamada front

---
+++ WEB/Controllers/ChamadaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB.Models;
+
+namespace WEB.Controllers {
+    public class ChamadaController(IConfiguration configuration) : Controller {
+        public IActionResult Index(bool icAdicionar = false) {
+            string? token = Request.Cookies["Token"];
+            if (string.IsNullOrEmpty(token)) {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var dados = JwtToken.DescriptografarJwt(token);
+            ViewBag.Role = dados.role[0];
+            ViewBag.Nome = dados.role[1];
+
+            ViewBag.IcAdicionar = icAdicionar;
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarAulas() {
+            return View("_ListaAulas");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CarregarInfoAula() {
+            return View("_InfoAula");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CarregarAdicionarArquivo() {
+            return View("_AdicionarArquivo");
+        }
+    }
+}
