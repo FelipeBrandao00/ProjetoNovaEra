e6a57847847d3cf851bed17f4120a21d44dcbfe9
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2024-11-15 20:00:59 -0300

    feat: matricula funcional

--- WEB/Controllers/MatriculaController.cs
+++ WEB/Controllers/MatriculaController.cs
@@ -7,6 +7,7 @@ using WEB.Models.Usuario;
 using WEB.Models.Aluno;
 using WEB.Models.Genero;
 using Application.Responses;
+using WEB.Models.Professor;
 
 namespace WEB.Controllers {
     public class MatriculaController(IConfiguration configuration) : Controller {
@@ -34,96 +35,32 @@ namespace WEB.Controllers {
 
             var MatriculaViewModel = new MatriculaViewModel();
             var ListaMatriculas = await MatriculaViewModel.BuscarPorTurma(configuration, ResponseModelMatricula);
-            ViewBag.ListaMatriculas = ListaMatriculas.Data;
+
+            var ListaMatriculasAtende = ListaMatriculas.Data.Where(x => x.icAtendeFiltro == true); 
+            ViewBag.ListaMatriculas = ListaMatriculasAtende;
+            ViewBag.QuantidadeAprovados = ListaMatriculasAtende.Count();
             return View("_ListaAlunos");
         }
 
         [HttpGet]
-        public async Task<IActionResult> CarregarInfoAluno(ResponseModelUsuario ResponseModelUsuario) {
+        public async Task<IActionResult> CarregarInfoAluno(MatriculaViewModel MatriculaViewModel) {
             configuration["JwtToken"] = Request.Cookies["Token"];
 
-            var UsuarioViewModel = new UsuarioViewModel();
-            UsuarioViewModel.DsCpf = ResponseModelUsuario.DsCpf;
-
-            var InfoUsuario = await UsuarioViewModel.BuscarInfo(configuration);
+            var InfoMatricula = await MatriculaViewModel.GetById(configuration, MatriculaViewModel.cdMatricula);
 
             var GeneroViewModel = new GeneroViewModel();
             var ListaGenero = await GeneroViewModel.GerarLista(configuration);
             ViewBag.ListaGenero = ListaGenero.Data;
 
-            return PartialView("_InfoAluno", InfoUsuario.Data);
-        }
-        
-        [HttpGet]
-        public async Task<IActionResult> CarregarMatriculaAluno() {
-            return View("_MatriculaAluno");
+            return PartialView("_InfoAluno", InfoMatricula.Data);
         }
 
-        //public async Task<IActionResult> CarregarAdicionarTurma() {
-        //    return PartialView("_AdicionarTurma", null);
-        //}
+        [HttpPost]
+        public async Task<JsonResult> ConfirmarMatriculas([FromBody] int[] idMatriculas) {
+            configuration["JwtToken"] = Request.Cookies["Token"];
+            var retornoConfirma = await new MatriculaViewModel().EfetivarMatriculas(configuration, idMatriculas);
 
-        //public async Task<IActionResult> AdicionarTurma(ResponseModelCurso ResponseModelCurso) {
-        //    configuration["JwtToken"] = Request.Cookies["Token"];
-        //    var responseAdd = await new CursoViewModel().Adicionar(configuration, ResponseModelCurso);
-
-        //    return PartialView("_InfoTurma", responseAdd.Data);
-        //}
-
-        //[HttpPost]
-        //public async Task<bool> AtualizarInfoTurma(ResponseModelCurso ResponseModelCurso) {
-        //    configuration["JwtToken"] = Request.Cookies["Token"];
-        //    var response = await new CursoViewModel().AtualizarInfo(configuration, ResponseModelCurso);
-        //    return response.IsSuccess;
-        //}
-
-        //[HttpPost]
-        //public async Task<bool> ReativarTurma(ResponseModelCurso ResponseModelCurso) {
-        //    configuration["JwtToken"] = Request.Cookies["Token"];
-        //    var response = await new CursoViewModel().Reativar(configuration, ResponseModelCurso);
-        //    return response.IsSuccess;
-        //}
-
-        //[HttpPost]
-        //public async Task<bool> FinalizarTurma(ResponseModelCurso ResponseModelCurso) {
-        //    configuration["JwtToken"] = Request.Cookies["Token"];
-        //    var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return response.IsSuccess;
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> CarregarAulasTurma() {
-        //    //configuration["JwtToken"] = Request.Cookies["Token"];
-        //    //var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return PartialView("_ListaAulas");
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> CarregarAlunosTurma() {
-        //    //configuration["JwtToken"] = Request.Cookies["Token"];
-        //    //var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return PartialView("_ListaAlunos");
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> CarregarInfoAula() {
-        //    //configuration["JwtToken"] = Request.Cookies["Token"];
-        //    //var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return PartialView("_InfoAula");
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> AdicionarAula() {
-        //    //configuration["JwtToken"] = Request.Cookies["Token"];
-        //    //var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return PartialView("_AdicionarAula");
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> CarregarInfoAluno() {
-        //    //configuration["JwtToken"] = Request.Cookies["Token"];
-        //    //var response = await new CursoViewModel().Finalizar(configuration, ResponseModelCurso);
-        //    return PartialView("_InfoAluno");
-        //}
+            return Json(retornoConfirma);
+        }
     }
 }
